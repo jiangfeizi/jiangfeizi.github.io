730388bb6216bb57b25aa276a6d4b507a368c217
Tom <1131066623@qq.com>
2022-04-09 16:10:48 +0800

    init

---
+++ pelicanconf.py
@@ -0,0 +1,30 @@
+AUTHOR = 'Tom'
+SITENAME = 'Blog'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Rome'
+
+DEFAULT_LANG = 'en'
+USE_FOLDER_AS_CATEGORY = True
+SLUGIFY_SOURCE = 'title'
+DEFAULT_DATE = 'fs'
+FILENAME_METADATA = '(?P<title>.*)'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+GITHUB_URL = 'https://github.com/jiangfeizi'
+# LINKS = (('GitHub', 'https://github.com/jiangfeizi'),)
+
+DEFAULT_PAGINATION = 10
+PORT = 6006
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
